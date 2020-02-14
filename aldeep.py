4356a31d82d0abc92ab855de25ace933e7e156f4
alannguyencs <alannguyen.cs@gmail.com>
2020-02-14 22:08:02 +0800

    vireo

--- aldeep.py
+++ aldeep.py
@@ -1,9 +1,15 @@
 import torch.nn.functional as F
+import torch
 
 
 def cross_entropy_loss2d(input, target, weight=None):
     log_p = F.log_softmax(input)    
     return F.nll_loss(log_p, target, weight=weight)
+
+def get_class_weight(mask):
+	MASK_SIZE = 256
+	class_weight = (torch.sum(mask)).item() / (MASK_SIZE * MASK_SIZE * mask.size(0))
+	return torch.tensor([class_weight, 1.0 - class_weight])
 
 def explore_architecture(model):
     child_counter = 0
--- allog.py
+++ allog.py
@@ -1,18 +1,37 @@
-from . import altime
+# from . import altime
 
-def update_content(file_path, content):
-	log_file = open(file_path, 'a')
-	log_file.write(content + '\n')
-	log_file.close()
+# def update_content(file_path, content):
+# 	log_file = open(file_path, 'a')
+# 	log_file.write(content + '\n')
+# 	log_file.close()
+
+# class Allog():
+# 	def __init__(self, log_file_path):
+# 		self.log_file_path = log_file_path
+
+# 		time_stamp = altime.get_timestamp()
+# 		header_content = '\n' * 3 + '-' * 100 + '\n' + time_stamp
+# 		update_content(self.log_file_path, header_content)
+
+# 	def update(self, content):
+# 		print (content)
+# 		update_content(self.log_file_path, content)
+from sys import stdout
+import logging
 
 class Allog():
 	def __init__(self, log_file_path):
-		self.log_file_path = log_file_path
+		logging.basicConfig(filename=log_file_path,
+                            filemode='a',
+                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%H:%M:%S',
+                            level=logging.INFO)
 
-		time_stamp = altime.get_timestamp()
-		header_content = '\n' * 3 + '-' * 100 + '\n' + time_stamp
-		update_content(self.log_file_path, header_content)
+		self.log = logging.getLogger()
+		formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+		handler = logging.StreamHandler(stdout)
+		handler.setFormatter(formatter)
+		self.log.addHandler(handler)
 
-	def update(self, content):
-		print (content)
-		update_content(self.log_file_path, content)
+	def info(self, message):
+		self.log.info(message)
