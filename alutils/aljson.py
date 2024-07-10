5a51e36955cb8c5b0c8a151c46145e3b5eb33e48
Nguyen Huu Thanh <alannguyen.cs@gmail.com>
2024-07-10 15:55:56 +0800

    fix: load json file

--- alutils/aljson.py
+++ alutils/aljson.py
@@ -2,7 +2,7 @@ import json
 
 
 def load(json_path):
-    with open(json_path) as json_file:
+    with open(json_path, 'r', encoding='utf-8') as json_file:
         return json.load(json_file)
 
 def save(json_data, json_path):
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
 setup(
 	name='alutils',
+    version="1.1.2407",
 	packages=find_packages(),
 )
